a9f5a0b81caa82b64469ba8b0c7c171bacf03c15
Marcin Barszcz <marcin.barszcz@op.pl>
2022-10-15 10:51:58 +0200

    feature: styled users list

---
+++ src/components/atoms/Button/Button.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  width: 25px;
+  height: 25px;
+  margin: 0 10px;
+  background-color: ${({ theme }) => theme.colors.grey};
+  border-radius: 50px;
+  border: none;
+  color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
