65f0e6ff7076be3bab089add31439b1efcc1c258
Marcin Barszcz <marcin.barszcz@op.pl>
2022-10-18 20:50:47 +0200

    feature: input, form, routing

--- src/components/atoms/DeleteButton/DeleteButton.styles.js
+++ src/components/atoms/DeleteButton/DeleteButton.styles.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
-  width: 25px;
-  height: 25px;
+  width: 22px;
+  height: 22px;
   margin: 0 10px;
   background-color: ${({ theme }) => theme.colors.grey};
   border-radius: 50px;
   border: none;
-  color: ${({ theme }) => theme.colors.white};
+  color: white;
   display: flex;
   justify-content: center;
   align-items: center;
