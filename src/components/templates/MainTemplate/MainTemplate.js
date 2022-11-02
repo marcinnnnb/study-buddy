5d142441f691024e9a49045d3345dd4b50feeff1
Marcin Barszcz <marcin.barszcz@op.pl>
2022-11-02 21:47:37 +0100

    feature: mock service worker & route parameters added

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navigation from 'components/organisms/Navigation/Navigation';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
 import { SearchBar } from 'components/organisms/SearchBar/SearchBar';
-import NewsSection from '../NewsSection/NewsSections';
+import NewsSection from 'components/templates/NewsSection/NewsSection';
 
 const MainTemplate = ({ children }) => {
   return (
