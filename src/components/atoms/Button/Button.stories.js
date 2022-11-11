e7e10bf6c742a131b75dbf34b7e8f833f6a328fa
Marcin Barszcz <marcin.barszcz@op.pl>
2022-11-11 13:16:10 +0100

    feature: storybook added for few components

---
+++ src/components/atoms/Button/Button.stories.js
@@ -0,0 +1,13 @@
+import { Button } from './Button';
+
+export default {
+  title: 'Components/Atoms/Button',
+  component: Button,
+};
+
+const Template = (args) => <Button {...args}>Read more</Button>;
+
+export const Default = Template.bind({});
+Default.args = {
+  isBig: false,
+};
